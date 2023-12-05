6a933b4c7488f104094ade4e400509a3679308bb
miglehook <145342534+miglehook@users.noreply.github.com>
2023-12-05 12:04:08 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Xlozt Hub'
+	SaveManager.Folder = 'LinoriaLibSettings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
